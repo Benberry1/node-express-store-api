9f4ae0d6523549e3903a11e8803001d9bc8c2c04
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-06-09 16:15:37 +1200

    add middleware to app.js, connect to mongo server.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const notFoundMiddleware = require("./middleware/notFound");
+const errorMiddleware = require("./middleware/errorHandler");
 
+// middleware
+app.use(express.json());
+
+// routes
 app.get("/", (req, res) => {
-  res.status(200).send("Testing");
+  res
+    .status(200)
+    .send('<h1>Store API</h1><a href="/api/v1/products">Products</a>');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5001;
+const connectDB = require("./db/connect");
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
