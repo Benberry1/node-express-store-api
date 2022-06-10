bad4dcabb8197ba6473346754b7fef5c2cd8f441
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-06-10 13:11:26 +1200

    create mongoose model for products

---
+++ models/products.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const ProductModel = mongoose.model("product", productSchema);
+
+module.exports = ProductModel;
